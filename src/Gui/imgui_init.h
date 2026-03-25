b22059ecd4581a62a8586c2ee92460d5498b0097
arieldk1212 <arieldk6@gmail.com>
2026-03-25 21:28:20 +0200

    imgui

--- src/Gui/imgui_init.h
+++ src/Gui/imgui_init.h
@@ -3,8 +3,45 @@
 
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
+#include <GLFW/glfw3.h>
 
 // TODO: add imgui menu, control with button the color of the cubes
+
+class Gui {
+public:
+  Gui(GLFWwindow *window) {
+    float main_scale =
+        ImGui_ImplGlfw_GetContentScaleForMonitor(glfwGetPrimaryMonitor());
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    (void)io;
+    io.ConfigFlags |=
+        ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+    io.ConfigFlags |=
+        ImGuiConfigFlags_NavEnableGamepad; // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    // ImGui::StyleColorsLight();
+
+    // Setup scaling
+    ImGuiStyle &style = ImGui::GetStyle();
+    style.ScaleAllSizes(
+        main_scale); // Bake a fixed style scale. (until we have a solution for
+                     // dynamic style scaling, changing this requires resetting
+                     // Style + calling this again)
+    style.FontScaleDpi =
+        main_scale; // Set initial font scale. (in docking branch: using
+                    // io.ConfigDpiScaleFonts=true automatically overrides this
+                    // for every window depending on the current monitor)
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+  }
+  ~Gui();
+
+private:
+};
 
 #endif
